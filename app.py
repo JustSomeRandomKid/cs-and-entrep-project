57abe7e08a8e363dbebe9ae7fc69be1e572bbcb4
JustSomeRandomKid <127970878+JustSomeRandomKid@users.noreply.github.com>
2024-07-25 15:12:36 +0300

    firebase setup

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from flask import Flask, render_template,request,redirect,url_for
 from flask import session as login_session
 import pyrebase
-import math
-import time
-import random
-import threading
+
 
 firebaseConfig = {
-
+  "apiKey": "AIzaSyDZ4pS2tY95eqtxLsqUCOllfePwBjtOkfE",
+  "authDomain": "trash-project-d71b5.firebaseapp.com",
+  "projectId": "trash-project-d71b5",
+  "storageBucket": "trash-project-d71b5.appspot.com",
+  "messagingSenderId": "986872772198",
+  "appId": "1:986872772198:web:d03f739984ad4644f55fa7",
+  'databaseURL':"https://trash-project-d71b5-default-rtdb.europe-west1.firebasedatabase.app/"
 }
 firebase = pyrebase.initialize_app(firebaseConfig)
 auth = firebase.auth()
