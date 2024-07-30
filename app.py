54499a314c427e29d713a23955a837a58fd7cc7b
JustSomeRandomKid <127970878+JustSomeRandomKid@users.noreply.github.com>
2024-07-30 10:59:02 +0300

    second version project - basic about and our book

--- app.py
+++ app.py
@@ -20,10 +20,60 @@ app.config['SECRET_KEY']="CS forever"
 
 db =firebase.database()
 
-@app.route('/home')
-@app.route('/')
+@app.route('/home',methods=['GET','POST'])
+@app.route('/',methods=['GET','POST'])
 def home():
-    return render_template('home.html')
+    if request.method=='GET':
+      try:
+        email=login_session['user']['email']
+      except:
+        email='Guest'
+      return render_template('home.html',email = email)
+    booksOwned=db.child('carts').child(login_session['user']['localId']).child('book').get().val()
+    db.child('carts').child(login_session['user']['localId']).child('book').set(booksOwned+1)
+    return redirect(url_for('home'))
+        
+
+
+
+@app.route('/signIn',methods=['GET','POST'])
+def signIn():
+    if request.method == 'GET':
+        return render_template('signIn.html')
+    else:
+        email = request.form['email']
+        password = request.form['password']
+        login_session['quotes'] = []
+        try:
+            login_session['user'] = auth.sign_in_with_email_and_password(email,password)
+        except Exception as e:
+            print("Error:",e)
+        return redirect(url_for('home'))
+    
+@app.route('/signUp',methods=['GET','POST'])
+def signUp():
+    if request.method == 'GET':
+        return render_template('signUp.html')
+    email = request.form['email']
+    password = request.form['password']
+    login_session['quotes'] = []
+    try:
+        login_session['user'] = auth.create_user_with_email_and_password(email,password)
+        print(login_session['user']['localId'])
+        db.child('carts').child(login_session['user']['localId']).set({"book":0})
+    except Exception as e:
+        print("Error:",e)
+
+    return redirect(url_for('home'))
+
+
+@app.route('/signOut')
+def signOut():
+    auth.current_user = None
+    login_session['user']=None
+    return redirect(url_for('home'))
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
